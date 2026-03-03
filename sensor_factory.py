907fba92c03084a7d624b8066f4881aa094dc1d7
Xavier Guinness Stout <xstout@mtech.edu>
2026-03-03 13:51:52 -0700

    added redundancy for ads

--- sensor_factory.py
+++ sensor_factory.py
@@ -14,11 +14,10 @@ class SensorFactory:
     @staticmethod
     def create_sensor(config) -> TemperatureSensor:
         mode = config.get("mode")
+        pin = config.get("pin", 21)
+        chip = config.get("chip", 0)
         if mode == "dht11":
-            pin = config.get("pin", 21)
-            chip = config.get("chip", 0)
             gpio_handle = lgpio.gpiochip_open(chip)
-
             # wrap these in retry Decorator class 
             primary = RetryDecorator(DHTAdapter(pin=pin, gpio_handle=gpio_handle), retries=3)
             secondary = RetryDecorator(ADSAdapter(), retries=3)
@@ -28,7 +27,12 @@ class SensorFactory:
 
         # warp ADS sensor in retry Decorator object
         elif mode == "ads":
-            return RetryDecorator(ADSAdapter(), retries=3)
+            gpio_handle = lgpio.gpiochip_open(chip)
+            # wrap these in retry Decorator class 
+            primary = RetryDecorator(ADSAdapter(), retries=3)
+            secondary = RetryDecorator(DHTAdapter(pin=pin, gpio_handle=gpio_handle), retries=3)
+            # return FallbackDecorator object with gpio Handle
+            return RetryDecorator(primary, secondary, gpio_handle=gpio_handle)
 
         # Prolly config file is wrong, so make sure mode is correct in config.json
         else:
